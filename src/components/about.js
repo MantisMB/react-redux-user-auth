ecc510d1b3afae964a7e687c2789fb5b716d7455
mantismb <matt.d.baldwin@gmail.com>
2019-10-07 21:01:04 -0700

    first step

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">About Us</h1>
+        <p>Lorem ipsum about sit amet
+        </p>
+        <p>Lorem ipsum about sit amet
+        </p>
+        <p>Lorem ipsum about sit amet
+        </p>
+    </div>
+);
---
+++ src/components/operatives_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Operatives List</h1>
+        <ol>
+            <li>Bob Ross</li>
+            <li>Bruce Wayne</li>
+            <li>Jim Rhodes</li>
+            <li>Harry Potter</li>
+            <li>Walt Disney</li>
+        </ol>  
+    </div>
+);
